0d17cecaa7dbb815106b30208e95db38046d9598
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-06-30 02:58:11 -0500

    feat: auth validation

---
+++ src/middlewares/auth.middewares.ts
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import userRepositories from "@/repositories/users.repositories";
+import { Request, Response, NextFunction } from "express";
+import errorCase from "@/errors/errors";
+dotenv.config();
+
+
+export async function authorization(req:Request, res:Response, next:NextFunction ) {
+	const { authorization } = req.headers;
+	const token = authorization?.replace("Bearer ", "");
+
+	if (!token) return errorCase.unauthorizedError("Token não encontrado!");
+
+    const userInfo = jwt.verify(token, process.env.SECRET_KEY);
+
+    let id:string;
+
+    if (typeof userInfo === "string") throw errorCase.unauthorizedError("Token Inválido ou Expirado.");
+    
+    id = userInfo.id;
+
+    const user = await userRepositories.userLoggedDB(Number(id), token);
+
+    if (user.rowCount === 0) throw errorCase.unauthorizedError("Token expirado ou inválido.");
+    
+    res.locals.user = userInfo;
+    next();
+}
