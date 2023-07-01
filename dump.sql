e93ae03d81256138e45f709dca24a1fb3ad1433e
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-07-01 04:04:41 -0500

    fix: dump sql

--- dump.sql
+++ dump.sql
@@ -21,13 +21,130 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: _prisma_migrations; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public._prisma_migrations (
+    id character varying(36) NOT NULL,
+    checksum character varying(64) NOT NULL,
+    finished_at timestamp with time zone,
+    migration_name character varying(255) NOT NULL,
+    logs text,
+    rolled_back_at timestamp with time zone,
+    started_at timestamp with time zone DEFAULT now() NOT NULL,
+    applied_steps_count integer DEFAULT 0 NOT NULL
+);
+
+
+--
+-- Name: comments; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.comments (
+    id integer NOT NULL,
+    movie_id integer NOT NULL,
+    user_id integer NOT NULL,
+    comment text NOT NULL
+);
+
+
+--
+-- Name: comments_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.comments_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: comments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.comments_id_seq OWNED BY public.comments.id;
+
+
+--
+-- Name: movies; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.movies (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    title text NOT NULL,
+    synopsis text NOT NULL,
+    date timestamp(3) without time zone NOT NULL,
+    genre text NOT NULL,
+    poster text NOT NULL,
+    watched boolean DEFAULT false NOT NULL
+);
+
+
+--
+-- Name: movies_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.movies_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: movies_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.movies_id_seq OWNED BY public.movies.id;
+
+
+--
+-- Name: ratings; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.ratings (
+    id integer NOT NULL,
+    movie_id integer NOT NULL,
+    user_id integer NOT NULL,
+    "like" boolean DEFAULT false NOT NULL,
+    unlike boolean DEFAULT false NOT NULL
+);
+
+
+--
+-- Name: ratings_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.ratings_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: ratings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.ratings_id_seq OWNED BY public.ratings.id;
+
+
+--
 -- Name: sessions; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.sessions (
     id integer NOT NULL,
-    user_id integer,
-    token text NOT NULL
+    token text NOT NULL,
+    user_id integer NOT NULL
 );
 
 
@@ -57,8 +174,9 @@ ALTER SEQUENCE public.sessions_id_seq OWNED BY public.sessions.id;
 
 CREATE TABLE public.users (
     id integer NOT NULL,
-    username text NOT NULL,
     email text NOT NULL,
+    username text NOT NULL,
+    image text NOT NULL,
     password text NOT NULL
 );
 
@@ -84,6 +202,27 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: comments id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments ALTER COLUMN id SET DEFAULT nextval('public.comments_id_seq'::regclass);
+
+
+--
+-- Name: movies id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.movies ALTER COLUMN id SET DEFAULT nextval('public.movies_id_seq'::regclass);
+
+
+--
+-- Name: ratings id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.ratings ALTER COLUMN id SET DEFAULT nextval('public.ratings_id_seq'::regclass);
+
+
+--
 -- Name: sessions id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -98,18 +237,67 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
+-- Data for Name: _prisma_migrations; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public._prisma_migrations VALUES ('76240ab3-e850-4e17-9c54-986c30a197a1', '61f57773f70850cc184353b4af2b9369cf1c1602c2c1dc0e9707282c43259560', '2023-07-01 03:26:18.635969-05', '20230701082617_init', NULL, NULL, '2023-07-01 03:26:17.926069-05', 1);
+
+
+--
+-- Data for Name: comments; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- Data for Name: movies; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+INSERT INTO public.movies VALUES (1, 1, 'Click', 'Um arquiteto casado e com filhos está cada vez mais frustrado por passar a maior parte de seu tempo trabalhando. Um dia, ele encontra um inventor excêntrico que lhe dá um controle remoto universal, com capacidade de acelerar o tempo. No início, ele usa o aparelho para acelerar qualquer momento tedioso, mas se dá conta de que está acelerando o tempo demais e deixando de viver preciosos momentos em família. Desesperado, ele procura o inventor para ajudá-lo a reverter o que fez.', '2006-01-01 00:00:00', 'Comédia,Romance, Drama, Fantasia', 'https://macmagazine.com.br/wp-content/uploads/2013/09/19-click.jpg', false);
+INSERT INTO public.movies VALUES (2, 2, 'Click', 'Um arquiteto casado e com filhos está cada vez mais frustrado por passar a maior parte de seu tempo trabalhando. Um dia, ele encontra um inventor excêntrico que lhe dá um controle remoto universal, com capacidade de acelerar o tempo. No início, ele usa o aparelho para acelerar qualquer momento tedioso, mas se dá conta de que está acelerando o tempo demais e deixando de viver preciosos momentos em família. Desesperado, ele procura o inventor para ajudá-lo a reverter o que fez.', '2006-01-01 00:00:00', 'Comédia,Romance, Drama, Fantasia', 'https://macmagazine.com.br/wp-content/uploads/2013/09/19-click.jpg', false);
+
+
+--
+-- Data for Name: ratings; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
 -- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.sessions VALUES (2, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJTdXdlaWthaSIsImlhdCI6MTY4ODE0ODYzNiwiZXhwIjoxNjg4MTUyMjM2fQ._0DBx8qELhULeNvYJoM7FYsimKtq5wHwzxFxQMpsiSQ');
+INSERT INTO public.sessions VALUES (1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJSb2RyaWdvIFNhbiIsImlhdCI6MTY4ODIwMDgxMCwiZXhwIjoxNjg4MjA0NDEwfQ.BZhlAlx8aYGfBMLNE4arVRENz_9YXW4n619pOSS6Tj0', 1);
+INSERT INTO public.sessions VALUES (2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJTdXdlaWthaSIsImlhdCI6MTY4ODIwMTA3OSwiZXhwIjoxNjg4MjA0Njc5fQ.7GySt7ki5GLwI_K3_Km9ALJ3oJ5beEyXuDg-O2SoHow', 2);
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.users VALUES (1, 'Suweikai', 'suweikai@china.com', '$2b$10$YR4dSHCYABmbo00aCTPinuPwSuaAzSJSWkQm/Hcu1QY9pcCFUdVPu');
-INSERT INTO public.users VALUES (9, 'Rodrigo', 'rodrigo@rodrigo.com', '$2b$10$qGj/DiFTk48WbNrW/Y6Hw.bKlqaQL6LAKL/mAfTwEf3bpURd5Mj6q');
+INSERT INTO public.users VALUES (1, 'rodrigo@dev.com', 'Rodrigo San', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiw0gsF9DKe7TmNkOFFHHYRCKw-Bi3UFMfuw&usqp.png', '$2b$10$TchHff4ykBpho/NcycXifeERap7mvrKJEpm.3NijTIb3.EDvwAcBu');
+INSERT INTO public.users VALUES (2, 'suweikai@dev.com', 'Suweikai', 'https://2.bp.blogspot.com/-tYdvf2L6Rxw/URnM8FVAyqI/AAAAAAAA1kA/l6AANzsJRt8/s1600/%5BG.U.I.S.+H-S%5D+Ninja+Sentai+Kakuranger+07+%286F9ABB0F%29.mkv_snapshot_02.14_%5B2013.02.11_22.20.02%5D.jpg', '$2b$10$6xgaM8v0Wrh8YVoYEdr58.prOG7YUQPE5ZB.3r5M/U8jELPnNMvum');
+
+
+--
+-- Name: comments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.comments_id_seq', 1, false);
+
+
+--
+-- Name: movies_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.movies_id_seq', 2, true);
+
+
+--
+-- Name: ratings_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.ratings_id_seq', 1, false);
 
 
 --
@@ -123,7 +311,39 @@ SELECT pg_catalog.setval('public.sessions_id_seq', 2, true);
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 9, true);
+SELECT pg_catalog.setval('public.users_id_seq', 2, true);
+
+
+--
+-- Name: _prisma_migrations _prisma_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public._prisma_migrations
+    ADD CONSTRAINT _prisma_migrations_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: comments comments_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: movies movies_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.movies
+    ADD CONSTRAINT movies_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: ratings ratings_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT ratings_pkey PRIMARY KEY (id);
 
 
 --
@@ -135,14 +355,6 @@ ALTER TABLE ONLY public.sessions
 
 
 --
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-
---
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -151,11 +363,58 @@ ALTER TABLE ONLY public.users
 
 
 --
+-- Name: users_email_key; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX users_email_key ON public.users USING btree (email);
+
+
+--
+-- Name: comments comments_movie_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(id) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- Name: comments comments_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- Name: movies movies_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.movies
+    ADD CONSTRAINT movies_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- Name: ratings ratings_movie_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT ratings_movie_id_fkey FOREIGN KEY (movie_id) REFERENCES public.movies(id) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
+-- Name: ratings ratings_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.ratings
+    ADD CONSTRAINT ratings_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE RESTRICT;
+
+
+--
 -- Name: sessions sessions_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.sessions
-    ADD CONSTRAINT sessions_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+    ADD CONSTRAINT sessions_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE RESTRICT;
 
 
 --
