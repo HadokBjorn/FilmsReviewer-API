60c836f006ad99086630ed34b0a3580dc19bdd16
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-06-30 22:09:02 -0500

    fix: index routers

--- src/routers/index.routes.ts
+++ src/routers/index.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import userRouters from "./users.routes";
+import movieRouters from "./movies.routes";
 
 const routers = Router();
 routers.use(userRouters)
-
+routers.use(movieRouters)
 export default routers;
