5f6dea5b42a0497088887b465ffd9e8ed83164e6
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-06-30 22:08:29 -0500

    feat: users routers

--- src/routers/users.routes.ts
+++ src/routers/users.routes.ts
@@ -1,7 +1,7 @@
 import userControllers from "@/controllers/users.controllers";
 import { validateLogin } from "@/middlewares/users.middlewares";
 import validateSchema from "@/middlewares/validateSchema.middlewares";
-import { loginSchema, signupSchema } from "@/schemas/users.schema";
+import { loginSchema, signupSchema } from "@/schemas/users.schemas";
 import { Router } from "express";
 
 const userRouters = Router();
