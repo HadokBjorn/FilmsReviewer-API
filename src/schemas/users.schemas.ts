31d05d29e2fe131ad8c0c0a2bb1761a42632c6ca
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-06-30 22:07:19 -0500

    feat: users schema

---
+++ src/schemas/users.schemas.ts
@@ -0,0 +1,19 @@
+import { User } from "@/types/users.types";
+import joi from "joi"
+
+export const signupSchema = joi.object<User>({
+	username: joi.string().min(3).required(),
+	email: joi
+		.string()
+		.email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "br"] } })
+		.required(),
+	password: joi.string().min(6).required(),
+});
+
+export const loginSchema = joi.object<User>({
+	email: joi
+		.string()
+		.email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "br"] } })
+		.required(),
+	password: joi.string().min(6).required(),
+});
