c7247868d10735a1c969e094f551a6f501893a03
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-06-30 22:08:44 -0500

    feat: movie routers

---
+++ src/routers/movies.routes.ts
@@ -0,0 +1,12 @@
+import movieControllers from "@/controllers/movies.controllers";
+import { authorization } from "@/middlewares/auth.middewares";
+import validateSchema from "@/middlewares/validateSchema.middlewares";
+import { movieSchema } from "@/schemas/movies.schemas";
+import { Router } from "express";
+
+const movieRouters = Router();
+
+movieRouters.post("/movies", validateSchema(movieSchema),authorization, movieControllers.createMovie)
+movieRouters.get("/movies", movieControllers.getMovies)
+
+export default movieRouters;
