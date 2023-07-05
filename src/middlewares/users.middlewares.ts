dc8d62fb39cd298dc2e6eba0ebcc7aaeb3cc9519
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-07-04 22:07:20 -0500

    feat: user middleware

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -3,23 +3,24 @@ import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import usersRepositories from "@/repositories/users.repositories"
 import { Request, Response, NextFunction } from "express";
-import httpStatus from "http-status";
 import errorCase from "@/errors/errors";
+import { User } from "@prisma/client";
+import userRepositories from "@/repositories/users.repositories";
 
 dotenv.config();
 
-export async function validateLogin(req:Request, res:Response, next: NextFunction) {
+async function validateLogin(req:Request, res:Response, next: NextFunction) {
 	const { email, password } = req.body;
     
     const user = await usersRepositories.getUserByEmailDB(email);
 
-    if (user.rowCount === 0) throw errorCase.unauthorizedError("Email or password invalid.");
+    if (!user || user.length > 1) throw errorCase.unauthorizedError("Email or password invalid.");
 
-    const correctPassword = bcrypt.compareSync(password, user.rows[0].password);
+    const correctPassword = bcrypt.compareSync(password, user[0].password);
 
     if (!correctPassword) throw errorCase.unauthorizedError("Email or password invalid.");
 
-    const { id, username, image} = user.rows[0];
+    const { id, username, image} = user[0];
     const oneHour = 3600; //one hour in seconds
     const token = jwt.sign({ id, username }, process.env.SECRET_KEY, { expiresIn: oneHour });
     await usersRepositories.deleteExpiredSessionDB(id, token);
@@ -28,3 +29,18 @@ export async function validateLogin(req:Request, res:Response, next: NextFunctio
     
     next();
 }
+
+async function validateSignup(req:Request, res:Response, next: NextFunction) {
+    const {email} = req.body as User
+	const duplicateEmail = await userRepositories.getUserByEmailDB(email)
+    
+    if(duplicateEmail.length > 1) throw errorCase.conflictError("user")
+    
+    next();
+}
+
+const userMiddleware = {
+    validateLogin,
+    validateSignup
+}
+export default userMiddleware;
