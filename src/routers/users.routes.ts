b96aff3ec39a6bbb42ebf83da63d71c6b1fa838b
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-07-04 22:09:30 -0500

    fix: update user router

--- src/routers/users.routes.ts
+++ src/routers/users.routes.ts
@@ -1,5 +1,5 @@
 import userControllers from "@/controllers/users.controllers";
-import { validateLogin } from "@/middlewares/users.middlewares";
+import userMiddleware from "@/middlewares/users.middlewares";
 import validateSchema from "@/middlewares/validateSchema.middlewares";
 import { loginSchema, signupSchema } from "@/schemas/users.schemas";
 import { Router } from "express";
@@ -7,6 +7,6 @@ import { Router } from "express";
 const userRouters = Router();
 
 userRouters.post("/signup",validateSchema(signupSchema), userControllers.signup)
-userRouters.post("/login",validateSchema(loginSchema),validateLogin, userControllers.login)
+userRouters.post("/login",validateSchema(loginSchema),userMiddleware.validateLogin, userControllers.login)
 
 export default userRouters;
